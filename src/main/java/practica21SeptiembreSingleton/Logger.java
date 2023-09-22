ff3cfba66ef817b6cbd95a8feee5fdef9aaf983d
s.corderoc <s.corderoc@jardinazuayo.fin.ec>
2023-09-21 20:45:46 -0500

    practica Singleton

---
+++ src/main/java/practica21SeptiembreSingleton/Logger.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practica21SeptiembreSingleton;
+
+/**
+ *
+ * @author s.corderoc
+ */
+public class Logger {
+        private static Logger instance;
+        private String texto;
+
+    public Logger() {
+    }
+        
+        public static Logger getInstance(){
+         if(instance == null){
+            instance = new Logger();
+        }
+        return instance;
+    }
+        
+                
+}
