082984a5311c519b4453f52a2441a2873607b413
ismaelm111 <ismaelm111@gmail.com>
2023-09-30 09:00:12 -0500

    implementacion strategy

--- src/main/java/practica30Strategy/PagoContext.java
+++ src/main/java/practica30Strategy/PagoContext.java
@@ -4,10 +4,31 @@
  */
 package practica30Strategy;
 
+import static practica30Strategy.FormaPago.EFECTIVO;
+
 /**
  *
  * @author Ismael
  */
 public class PagoContext {
-    
+    public String ejecutarPago(FormaPago pago){
+        String pagoGenerado;
+        switch (pago) {
+            case EFECTIVO:
+                var pagoEfectivo = new PagoEfectivo();
+                pagoGenerado = pagoEfectivo.generarComprobante();                
+                break;
+                case TC:
+                var pagoTC = new PagoTC();
+                pagoGenerado = pagoTC.generarComprobante();                
+                break;
+                case TRANSFERENCIA:
+                var pagoTransferencia = new PagoTransferencia();
+                pagoGenerado = pagoTransferencia.generarComprobante();                
+                break;
+            default:
+                pagoGenerado="pago no generado";
+        }
+        return pagoGenerado;
+    }
 }
---
+++ src/main/java/practica30Strategy/principal.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practica30Strategy;
+
+/**
+ *
+ * @author Ismael
+ */
+public class principal {
+    public static void main(String[] args) {
+        var pagoContext = new PagoContext();
+        var pagoGeneradoTC = pagoContext.ejecutarPago(FormaPago.TC);        
+        System.out.println(pagoGeneradoTC);
+        var pagoGeneradoEfectivo = pagoContext.ejecutarPago(FormaPago.EFECTIVO);
+        System.out.println(pagoGeneradoEfectivo);
+        var pagoGeneradoTansferencia = pagoContext.ejecutarPago(FormaPago.TRANSFERENCIA);
+        System.out.println(pagoGeneradoTansferencia);
+        
+        
+    }
+    
+}
