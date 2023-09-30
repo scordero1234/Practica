e3c6804eac96cafe3cd05b550fb9dcc95ad15fcf
ismaelm111 <ismaelm111@gmail.com>
2023-09-30 08:31:00 -0500

    agrega clases srategy

---
+++ src/main/java/practica30Strategy/PagoTC.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practica30Strategy;
+
+/**
+ *
+ * @author Ismael
+ */
+public class PagoTC implements PagoStrategy{
+
+    @Override
+    public String generarComprobante() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean procesarPago() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
